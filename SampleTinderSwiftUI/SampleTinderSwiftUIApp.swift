17ee7f5b67f3a5915fc5b42beffbaf070724d887
vfj-tanaka <108640688+vfj-tanaka@users.noreply.github.com>
2022-09-21 22:40:32 +0900

    Initial Commit

---
+++ SampleTinderSwiftUI/SampleTinderSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SampleTinderSwiftUIApp.swift
+//  SampleTinderSwiftUI
+//
+//  Created by mtanaka on 2022/09/21.
+//
+
+import SwiftUI
+
+@main
+struct SampleTinderSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
