2a6a76eb12fd436a08ec432a49eb9a66e9e6a525
vfj-tanaka <108640688+vfj-tanaka@users.noreply.github.com>
2022-09-22 11:56:57 +0900

    ep1

--- SampleTinderSwiftUI/ContentView.swift
+++ SampleTinderSwiftUI/ContentView.swift
@@ -8,14 +8,70 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    private var frameWidth: CGFloat {
+        UIScreen.main.bounds.width
+    }
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
+            HStack {
+                Button(action: {
+                    
+                }, label: {
+                    Image(systemName: "flame.fill")
+                        .resizable()
+                        .scaledToFill()
+                        .frame(width: 40, height: 40, alignment: .center)
+                        .foregroundColor(.red)
+                })
+                .frame(width: frameWidth / 4 - 20)
+                .padding(.leading, 30)
+                
+                Button(action: {
+                    
+                }, label: {
+                    Image(systemName: "suit.diamond.fill")
+                        .resizable()
+                        .scaledToFill()
+                    frame(width: 40, height: 40, alignment: .center)
+                        .foregroundColor(.yellow)
+                })
+                .frame(width: frameWidth / 4)
+                Button(action: {
+                    
+                }, label: {
+                    Image(systemName: "bubble.left.fill")
+                        .resizable()
+                        .scaledToFill()
+                    frame(width: 40, height: 40, alignment: .center)
+                        .foregroundColor(.orange)
+                })
+                .frame(width: frameWidth / 4)
+                Button(action: {
+                    
+                }, label: {
+                    Image(systemName: "person.crop.circle.fill")
+                        .resizable()
+                        .scaledToFill()
+                    frame(width: 40, height: 40, alignment: .center)
+                        .foregroundColor(.pink)
+                })
+                .frame(width: frameWidth / 4 - 20)
+                .padding(.trailing, 30)
+            }
+            .padding()
+            .frame(width: frameWidth)
+            
             Text("Hello, world!")
+                .padding()
+                .frame(width: frameWidth, height: 600)
+                .background(Color.blue)
+            Text("Hello, world!")
+                .padding()
+                .frame(width: frameWidth)
+                .background(Color.green)
         }
-        .padding()
     }
 }
 
